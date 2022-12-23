0acc1fa6873178893aaa7c9538c01eecf1226bc7
lsteidl <lsteidl@wisc.edu>
2022-12-23 10:22:17 -0600

    added some error handling and removed print statements to improve readability

--- main.go
+++ main.go
@@ -26,46 +26,38 @@ type item struct {
 	Price            string `json:"price"`
 }
 
+// receipts map, holds receipt submission data
+var receipts = map[string]receipt{}
+
 /*
 	Calculates total number of points for each category
 
-@param idFromRequest string indicating which receipt to process
+@param idFromRequest string indicating which receipt to process from receipts map
 @return int value of total rewared points
 */
 func calculatePoints(idFromRequest string) int {
-	// Handle situation of invalid ID requested, maybe earlier than this line
-	if receipts[idFromRequest].Retailer == "" {
-		fmt.Println("Bad Request, Receipt not Found")
-		return -101
-	}
-	fmt.Printf("Retailer is %s", receipts[idFromRequest].Retailer)
-	retailerPoints := 0 // Points earned for Retailer Name
-	roundPoints := 0    // Points earned for Round receipt total
+	retailerPoints := 0 // Points earned for retailer name
+	roundPoints := 0    // Points earned for round receipt total
 	multiplePoints := 0 // Points earned for receipt total being a multiple of (0.25)
 	itemPoints := 0     // Points earned for every 2 items
 	descPoints := 0     // Points earned from item description
 	datePoints := 0     // Points earned from purchase date
 	timePoints := 0     // Points earned from purchase time
+	totalPoints := 0    // Points earned from all categories combined
 
 	// One point for every alphanumeric character in the retailer name.
 	retailer := receipts[idFromRequest].Retailer
-	split := strings.Split(retailer, "") // convert Retailer Name into an array of strings, 1 letter per index
-	fmt.Printf("first letter: %s\n", split[0])
-	for i := 0; i < len(split); i++ { // iterate through each letter of the Retailer Name
+	split := strings.Split(retailer, "") // convert Retailer Name into a slice of strings, 1 letter per index
+	for i := 0; i < len(split); i++ {    // iterate through each letter of the Retailer Name
 		isAlphanumeric := regexp.MustCompile(`^[a-zA-Z0-9]*$`).MatchString(split[i]) // MustCompile parses a regular expression and returns a Regexp object, MatchString compares this object to "split[i]"
 		if isAlphanumeric {
-			fmt.Printf("%s is AlphaNumerical\n", split[i])
 			retailerPoints++
-		} else {
-			fmt.Printf("%s is NOT AlphaNumerical\n", split[i])
 		}
 	}
-	fmt.Printf("Total  Retailer Points: %d\n", retailerPoints)
 	// 50 points if the total is a round dollar amount with no cents.
-	fmt.Printf("Receipt Total is %s\n", receipts[idFromRequest].Total)
 	receiptTotal := receipts[idFromRequest].Total
-	isRoundDollar, err := regexp.MatchString(`^[0-9]*.[0]*$`, receiptTotal) // determine if receiptTotal matches the definition of "round dollar amount"
-	if isRoundDollar && err == nil {                                        // reward 50 points if string matches
+	isRoundDollar, err := regexp.MatchString(`^([0-9]*[.]*[0]*)$`, receiptTotal) // determine if receiptTotal matches the definition of "round dollar amount"
+	if isRoundDollar && err == nil {                                             // reward 50 points if string matches
 		roundPoints = 50
 	}
 	// 25 points if the total is a multiple of 0.25.
@@ -80,67 +72,52 @@ func calculatePoints(idFromRequest string) int {
 	//If the trimmed length of the item description is a multiple of 3, multiply the price by 0.2 and round up to the nearest integer.
 	//The result is the number of points earned.
 	for i := 0; i < len(receipts[idFromRequest].Items); i++ { //iterate through each item on the receipt
-		//fmt.Printf("Evaluating item %d...\n", i)
 		description := receipts[idFromRequest].Items[i].ShortDescription
 		price := receipts[idFromRequest].Items[i].Price
-		//fmt.Println(description)
 		descTrimmed := strings.TrimSpace(description) // trim leading and trailing spaces
-		//fmt.Println(descTrimmed)
-		trimmedLength := len(descTrimmed) // get length of trimmed Description
-		//fmt.Printf("Trimmed Length is %d\n", trimmedLength)
-		if trimmedLength%3 == 0 { // check if trimmed Length is a multiple of 3
-			fmt.Printf("Item %d - Trimmed Length is a multiple of 3\n", i)
-			//	fmt.Printf("price: %s\n", price)
+		trimmedLength := len(descTrimmed)             // get length of trimmed Description
+		if trimmedLength%3 == 0 {                     // check if trimmed Length is a multiple of 3
 			priceAsFloat, err := strconv.ParseFloat(price, 64)
 			if err == nil {
 				// Mulitply the price by 0.2 and round up to the nearest integer
 				pointsAsFloat := priceAsFloat * (0.2)           // multiply price by 0.2 to get points value
 				pointsAsInt := int(pointsAsFloat)               // convert to integer, dropping everything after the decimal
 				decimal := pointsAsFloat - float64(pointsAsInt) // store information after the decimal
-				//		fmt.Printf("priceAsFloat: %f\n", priceAsFloat)
-				//		fmt.Printf("priceAsInt: %d\n", priceAsInt)
-				//		fmt.Printf("decimal: %f\n", decimal)
-				if decimal != 0 { //if decimal information was discarded, add 1 to satsify the "round up" specification
-					//			fmt.Printf("Rounding up\n")
+				if decimal != 0 {                               //if decimal information was discarded, add 1 to satsify the "round up" specification
 					pointsAsInt++
 					descPoints += pointsAsInt
-				} else {
-					fmt.Printf("Already round!!!\n")
 				}
 			} else {
 				fmt.Println(err)
 			}
-			//	fmt.Printf("final modified priceAsInt: %d\n", priceAsInt)
-		} else {
-			fmt.Printf("Item %d - Trimmed Length is NOT a multiple of 3\n", i)
 		}
 	}
 
-	//6 points if the day in the purchase date is odd.
+	// 6 points if the day in the purchase date is odd. Assumes date format from examples (YYYY-MM-DD)
 	purchaseDate := receipts[idFromRequest].PurchaseDate
 	isOdd, err := regexp.MatchString(`^[0-9]*-[0-9]*-[0-3](1|3|5|7|9)$`, purchaseDate)
 	if isOdd && err == nil {
 		datePoints = 6
 	}
-	//10 points if the time of purchase is after 2:00pm and before 4:00pm. (14:01 to 15:59)
+	// 10 points if the time of purchase is after 2:00pm and before 4:00pm. (14:01 to 15:59)
 	purchaseTime := receipts[idFromRequest].PurchaseTime
 	isValidTime, err := regexp.MatchString(`^1[4-5]:[0-5][0-9]$`, purchaseTime)
 	if isValidTime && err == nil {
 		timePoints = 10
 	}
 	// Add all point categories
-	TotalPoints := retailerPoints + roundPoints + multiplePoints + itemPoints + descPoints + datePoints + timePoints
-	fmt.Printf("Total Points %d\n", TotalPoints)
-	fmt.Println("Points Breakdown...")
-	fmt.Printf("Retailer Points %d\n", retailerPoints)
-	fmt.Printf("Round Points %d\n", roundPoints)
-	fmt.Printf("Multiple Points %d\n", multiplePoints)
-	fmt.Printf("Item Points %d\n", itemPoints)
-	fmt.Printf("Desc Points %d\n", descPoints)
-	fmt.Printf("Date Points %d\n", datePoints)
-	fmt.Printf("Time Points %d\n", timePoints)
+	totalPoints = retailerPoints + roundPoints + multiplePoints + itemPoints + descPoints + datePoints + timePoints
+	fmt.Printf("Total Points: %d\n", totalPoints)
+	fmt.Println("Points Breakdown by Category...")
+	fmt.Printf("Round Points: %d\n", roundPoints)
+	fmt.Printf("Multiple Points: %d\n", multiplePoints)
+	fmt.Printf("Retailer Points: %d\n", retailerPoints)
+	fmt.Printf("Item Points: %d\n", itemPoints)
+	fmt.Printf("Desc Points: %d\n", descPoints)
+	fmt.Printf("Date Points: %d\n", datePoints)
+	fmt.Printf("Time Points: %d\n", timePoints)
 
-	return TotalPoints
+	return totalPoints
 }
 
 /*
@@ -150,13 +127,18 @@ func calculatePoints(idFromRequest string) int {
 */
 func getPoints(c *gin.Context) {
 	idFromRequest := c.Param("id")
-	points := calculatePoints(idFromRequest)
-	response := struct { // anonymous struct for the GET response
-		Points int `json:"points"`
-	}{
-		Points: points,
+	if receipts[idFromRequest].Retailer == "" { // Handle situation of unknown ID request, return error code 404
+		fmt.Println("Bad Request, Receipt not Found")
+		c.IndentedJSON(http.StatusNotFound, "Bad Request, Receipt not Found")
+	} else {
+		points := calculatePoints(idFromRequest)
+		response := struct { // anonymous struct for the GET response
+			Points int `json:"points"`
+		}{
+			Points: points,
+		}
+		c.IndentedJSON(http.StatusAccepted, response) // send the calculated point value
 	}
-	c.IndentedJSON(http.StatusCreated, response) // send the calculated point value
 }
 
 /*
@@ -167,24 +149,21 @@ func getPoints(c *gin.Context) {
 */
 func postReceipt(c *gin.Context) {
 	var newReceipt receipt
-	// Call BindJSON to bind the JSON data to newReceipt
-	if err := c.BindJSON(&newReceipt); err != nil {
-		fmt.Println("Error after binding", err)
-		return
+	err := c.BindJSON(&newReceipt)
+	if err != nil { // bind the JSON data to newReceipt
+		fmt.Println("Error after binding JSON data in postReceipt()", err)
+		c.IndentedJSON(http.StatusBadRequest, "Error processing data")
+	} else {
+		id := xid.New()                    // generate unique key to pass back in response
+		receipts[id.String()] = newReceipt // Add the new receipt to the map.
+		response := struct {               //anonymous struct for POST response
+			ID string `json:"id"`
+		}{
+			ID: id.String(),
+		}
+		c.IndentedJSON(http.StatusCreated, response)
 	}
-	// generate unique key to pass back in response
-	id := xid.New()                    // convert key to String
-	receipts[id.String()] = newReceipt // Add the new receipt to the slice.
-	response := struct {               //anonymous struct for POST response
-		ID string `json:"id"`
-	}{
-		ID: id.String(),
-	}
-	c.IndentedJSON(http.StatusCreated, response)
 }
-
-// receipts map, holds receipt submission data
-var receipts = map[string]receipt{}
 
 /*
 Provides 2 endpoints, Process Receipts (POST) and Get Points (GET)
